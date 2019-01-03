5c0a108b4b5a74e7cf19971db89b293060b29c1d
Lashin <lashin.sasha@gmail.com>
2019-01-03 23:22:08 +0300

    завершил верстку страницы about

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,15 +1,5 @@
 import "./modules/example";
 
 
-// Initialize and add the map
-export function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    //var map = new window.google.maps.Map(
-    //    document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    //var marker = new window.google.maps.Marker({position: uluru, map: map});
-  }
 
   

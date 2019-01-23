e9183e3d4b10acf67b8e9b0df12f9a1f59ac100c
Lashin <lashin.sasha@gmail.com>
2019-01-23 11:44:22 +0300

    +

--- src/assets/scripts/hamburger_menu.js
+++ src/assets/scripts/hamburger_menu.js
@@ -1,11 +1,25 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() { 
     const hamburgerMenu = document.getElementById('hamburger-menu');
+    const menu = document.getElementsByClassName('main-menu')[0];
+    const body = document.body;
     if(hamburgerMenu.classList.contains('is-active'))
     {
         hamburgerMenu.classList.remove('is-active');
+        body.classList.remove('overflow-nidden');
+        menu.childNodes.forEach((childNode) => {
+            if(childNode.nodeType === 1)
+                childNode.classList.remove('is-active');
+        })
     }
     else
     {
+        window.scroll(0, 0);
         hamburgerMenu.classList.add('is-active');
+        body.classList.add('overflow-nidden');
+
+        menu.childNodes.forEach((childNode) => {
+            if(childNode.nodeType === 1)
+                childNode.classList.add('is-active');
+        })
     }
  })

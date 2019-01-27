bdeb05dfacee519787f61db862e302c83b797c05
Lashin <lashin.sasha@gmail.com>
2019-01-27 13:29:31 +0300

    Добавил главное меню

--- src/assets/scripts/hamburger_menu.js
+++ src/assets/scripts/hamburger_menu.js
@@ -1,25 +1,27 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() { 
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const menu = document.getElementsByClassName('main-menu')[0];
+    var menuItems =  document.getElementsByClassName('main-menu-item');
     const body = document.body;
     if(hamburgerMenu.classList.contains('is-active'))
     {
         hamburgerMenu.classList.remove('is-active');
         body.classList.remove('overflow-nidden');
-        menu.childNodes.forEach((childNode) => {
-            if(childNode.nodeType === 1)
-                childNode.classList.remove('is-active');
-        })
+        menu.classList.remove('is-active');
+        for(var i = 0; i < menuItems.length; i++)
+        {
+            menuItems[i].style = '';
+        }
     }
     else
     {
         window.scroll(0, 0);
         hamburgerMenu.classList.add('is-active');
         body.classList.add('overflow-nidden');
-
-        menu.childNodes.forEach((childNode) => {
-            if(childNode.nodeType === 1)
-                childNode.classList.add('is-active');
-        })
+        menu.classList.add('is-active');
+        for(var i = 0; i < menuItems.length; i++)
+        {
+            menuItems[i].style.transitionDelay = (1 + i / 10) + 's';
+        }
     }
  })

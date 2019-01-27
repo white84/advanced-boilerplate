2dd7c610822c95d5d3bd4cae81e44fb8dec2ca2f
Lashin <lashin.sasha@gmail.com>
2019-01-27 15:33:37 +0300

    Доработал меню

--- src/assets/scripts/hamburger_menu.js
+++ src/assets/scripts/hamburger_menu.js
@@ -1,27 +1,37 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() { 
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const menu = document.getElementsByClassName('main-menu')[0];
-    var menuItems =  document.getElementsByClassName('main-menu-item');
+    const menuItems =  document.getElementsByClassName('main-menu-item');
+
     const body = document.body;
+    var i = 0;
     if(hamburgerMenu.classList.contains('is-active'))
     {
         hamburgerMenu.classList.remove('is-active');
         body.classList.remove('overflow-nidden');
-        menu.classList.remove('is-active');
-        for(var i = 0; i < menuItems.length; i++)
+        menu.classList.add('is-hidden');
+        for(i = 0; i < menuItems.length; i++)
         {
             menuItems[i].style = '';
         }
+        window.setTimeout(() => {
+            menu.classList.add('main-menu--hide');
+            menu.classList.remove('is-active');
+            menu.classList.remove('is-hidden');
+        }, 500);
     }
     else
     {
         window.scroll(0, 0);
         hamburgerMenu.classList.add('is-active');
         body.classList.add('overflow-nidden');
-        menu.classList.add('is-active');
-        for(var i = 0; i < menuItems.length; i++)
+        for(i = 0; i < menuItems.length; i++)
         {
             menuItems[i].style.transitionDelay = (1 + i / 10) + 's';
         }
+        menu.classList.remove('main-menu--hide');
+        window.setTimeout(() => {
+            menu.classList.add('is-active');
+        }, 100);
     }
  })
